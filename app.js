98b6d3e4ea873954943f9078f748c90b8d99e66c
liweids <18730640692@139.com>
2021-03-30 14:51:45 +0800

    '修连接改数据库配置文件'

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ app.use(bodyParser());
 
 app.use(router.routes());
 
-console.log("项目启动http://82.156.109.184:3000");
-console.log("项目启动http://localhost:3000");
-
 app.listen(3000);
 
 
 //CREATE USER '742314100'@'%' IDENTIFIED BY 'l4035564121';
 //grant all privileges on *.* to '742314100'@'%';
+//FLUSH PRIVILEGES;
+
+//GRANT ALL PRIVILEGES ON *.* TO '742314100'@'%' IDENTIFIED BY 'l4035564121' WITH GRANT OPTION;
--- config/DBConfig.js
+++ config/DBConfig.js
@@ -4,6 +4,7 @@ const database ={
     user: 'root', //mysql用户名
     password: 'admin', //mysql密码
     PORT: '3306', //mysql端口号
-    host: "127.0.0.1" //服务器ip
+    host: "127.0.0.1", //服务器ip
+    socketPath: '/tmp/mysql.sock' //解决 Error: connect ECONNREFUSED 127.0.0.1:3306
    }
    module.exports = database
